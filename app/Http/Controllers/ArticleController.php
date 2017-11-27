6fadad76fadefafeb70a7653c049a0403fecec9b
hugojos <hugosajama07@gmail.com>
2017-11-26 23:23:18 -0300

    Inicia el proyecto

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct(){
+      $this->middleware('auth');
+    }
+    public function index()
+    {
+        return view('article');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+      Post::create([
+        'title'=>$request->title,
+        'description'=>$request->description,
+        'price'=>$request->price,
+        'user_id'=>Auth::user()->id
+      ]);
+      return view('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        $das = $user->posts()->get();
+        return $das;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+
+    protected $fillable = [
+      'title','price','description','user_id'
+    ];
+
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+}
