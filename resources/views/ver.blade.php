35ac3c01b2c4f77c3613dcf596e597347b8f92fa
hugojos <hugosajama07@gmail.com>
2017-12-10 19:06:38 -0300

    cambios

--- resources/views/ver.blade.php
+++ resources/views/ver.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app1')
 @section('content')
   <div class="container misproductos">
-    <h1 style="margin-left: 20px;">Tus articulos</h1>
+    <h1 style="margin-left: 20px; text-decoration: underline;">Tus articulos</h1>
     @foreach ($articulos as $key => $value)
       <div class="articulo">
         <div class="izq">
+          <a href="/articles/{{$value->id}}" style="color:black; text-decoration:none;">
           <h1>{{$value->title}}</h1>
           <img src="/storage/products/{{$value->img1}}" style='width:20%'  alt="">
 
           <p>{{$value->description}} <span style="color:green;">${{$value->price}}</span></p>
+          </a>
         </div>
         <div class="der">
           <div class="forms">
