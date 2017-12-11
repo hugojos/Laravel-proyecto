3e2bcdfc2373ea4bdedd5a31b33840db0c3979ef
LeoNardelli01 <ricosur@gmail.com>
2017-12-10 22:16:06 -0300

    Arreglo del register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,70 +4,7 @@
 @extends('layouts.app1')
 
 @section('content')
-<<<<<<< HEAD
-<div class="container f2f2f2 register">
-    <div class="row  ">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Registrarse</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-                        <div class="form-group{{ $errors->has('alias') ? ' has-error' : '' }}">
-                            <label for="alias" class="col-md-4 control-label">Alias</label>
-
-                            <div class="col-md-6">
-                                <input id="alias" type="text" class="form-control" name="alias" value="{{ old('alias') }}" required>
-
-                                @if ($errors->has('alias'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('alias') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
-                            <label for="nombre" class="col-md-4 control-label">Nombre</label>
-
-                            <div class="col-md-6">
-                                <input id="nombre" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" required>
-
-                                @if ($errors->has('first_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('first_name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="apellido" class="col-md-4 control-label">Apellido</label>
-
-                            <div class="col-md-6">
-                                <input id="apellido" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autofocus>
-
-                                @if ($errors->has('last_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('last_name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-=======
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
   <style media="screen">
