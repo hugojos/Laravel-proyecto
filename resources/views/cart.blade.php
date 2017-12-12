ca43d92ae9d547a4efd9df4d21db69868653d815
hugojos <hugosajama07@gmail.com>
2017-12-12 16:22:16 -0300

    retoques a la vista cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,8 +8,7 @@
         text-align: center;
     }
     .cart-container {
-        margin-top: 83px;
-        margin: 10%;
+        width: 75%;
     }
     .prod-img {
         width: 50%;
@@ -17,52 +16,61 @@
     .img-row {
         width: 20%;
     }
+    .flex{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 88vh;
+      margin-top: 83px;
+    }
 </style>
 
-<section class="cart-container">
-
+<div class="flex">
+  <section class="cart-container">
     <h2 class="cart-title">Productos en tu carrito: {{Cart::content()->count()}}</h2>
     <table class="table">
-        <thead class="thead-light">
-            <tr>
-                <th scope="col"></th>
-                <th scope="col"></th>
-                <th scope="col">Código</th>
-                <th scope="col">Producto</th>
-                <th scope="col">Precio</th>
-            </tr>
-        </thead>
-        <tbody>
+      <thead class="thead-light">
+        <tr>
+          <th scope="col"></th>
+          <th scope="col"></th>
+          <th scope="col">Código</th>
+          <th scope="col">Producto</th>
+          <th scope="col">Precio</th>
+        </tr>
+      </thead>
+      <tbody>
 
-            @foreach (Cart::content() as $item)
-                <tr>
+        @foreach (Cart::content() as $item)
+          <tr>
 
-                    <td> <a class="btn btn-danger" href="{{route('deleteFromCart', ['id'=>$item->rowId]) }}">x</a> </td>
-                    <td class="img-row"><img class="prod-img" src="storage/products/{{$item->model->img2}} " alt="" srcset=""></td>
-                    <td>{{$item->id}}</td>
-                    <td>{{$item->name}} </td>
-                    <td>$ {{$item->price}}.00 </td>
-                </tr>
-            @endforeach
+            <td> <a class="btn btn-danger" href="{{route('deleteFromCart', ['id'=>$item->rowId]) }}">x</a> </td>
+            <td class="img-row"><img class="prod-img" src="storage/products/{{$item->model->img2}} " alt="" srcset=""></td>
+            <td>{{$item->id}}</td>
+            <td>{{$item->name}} </td>
+            <td>$ {{$item->price}}.00 </td>
+          </tr>
+        @endforeach
 
-            <tr>
-                <td></td>
-                <th scope="row"></th>
-                <td></td>
-                <td>TOTAL:</td>
-                <td>{{Cart::total()}} </td>
-            </tr>
-            <tr>
-                <td></td>
-                <th scope="row"></th>
-                <td></td>
-                <td> <a class="btn btn-success" href="{{ url('/categorias')}}">Seguir comprando</a> </td>
-                <td> <a class="btn btn-danger" href="{{route('checkout')}}">Finalizar la compra</a> </td>
-            </tr>
-        </tbody>
+        <tr>
+          <td></td>
+          <th scope="row"></th>
+          <td></td>
+          <td>TOTAL:</td>
+          <td>{{Cart::total()}} </td>
+        </tr>
+        <tr>
+          <td></td>
+          <th scope="row"></th>
+          <td></td>
+          <td> <a class="btn btn-success" href="{{ url('/categorias')}}">Seguir comprando</a> </td>
+          <td> <a class="btn btn-danger" href="{{route('checkout')}}">Finalizar la compra</a> </td>
+        </tr>
+      </tbody>
     </table>
 
-</section>
+  </section>
+
+</div>
 
 
 
