9b537d2310e8f23688694d25bbe34cc65af42688
alegroni <ajmarzano@gmail.com>
2017-12-14 13:01:33 -0300

    retoques al buscador

--- resources/views/buscador.blade.php
+++ resources/views/buscador.blade.php
@@ -5,11 +5,11 @@
     background: #f2f2f2;
     }
   </style>
-
-  <h1 style="margin-top: 89px; text-align:center;">Resultado de la busqueda: "{{$buscador}}"</h1>
-  <div class="container" style="margin-top: 44px; display:flex;flex-wrap:wrap;text-align:center; justify-content:center">
-    @foreach ($post as $product)
-      <div class="col-xs-6 col-sm-6 col-xl-4 articulo-vista" style="margin-bottom: 20px;">
+  @if (!count($post)== 0)
+    <h1 style="margin-top: 89px; text-align:center;">Resultado de la busqueda: "{{$buscador}}"</h1>
+    <div class="container" style="margin-top: 44px; display:flex;flex-wrap:wrap;text-align:center; justify-content:center">
+      @foreach ($post as $product)
+        <div class="col-xs-6 col-sm-6 col-xl-4 articulo-vista" style="margin-bottom: 20px;">
           <article class="articuloUno articulo">
             <div class="card">
               <img class="card-img-top" src="/storage/products/{{$product->img1}}" alt="Imagen producto uno" >
@@ -20,6 +20,12 @@
             </div>
           </article>
         </div>
-    @endforeach
-  </div>
+      @endforeach
+    </div>
+    @else
+      <div class="" style="min-height:101vh;display:flex;align-items:center;justify-content:center;">
+        <h1 style="text-align:center;">No se encontraron resultados de la busqueda :"{{$buscador}}"</h1>
+
+      </div>
+  @endif
 @endsection
