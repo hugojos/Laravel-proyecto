3ae182f850eb98eb05d216d4d8a18916bed7df29
hugojos <hugosajama07@gmail.com>
2017-12-14 20:58:00 -0300

    validacion javascript en formulario de registro hecha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,7 +60,7 @@
                   <div class="panel-heading text-center">REGISTRATE</div>
 
                   <div class="panel-body">
-                      <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                      <form class="form-horizontal" method="POST" action="{{ route('register') }}" id="formRegister">
                           {{ csrf_field() }}
                           <div class="form-group{{ $errors->has('alias') ? ' has-error' : '' }}">
                               <label for="alias" class="col-md-12 control-label">Alias</label>
@@ -68,11 +68,11 @@
                               <div class="col-md-12">
                                   <input id="alias" type="text" class="form-control" name="alias" value="{{ old('alias') }}" required>
 
+                                  <span class="help-block" style="color:red" id="errorAlias">
                                   @if ($errors->has('alias'))
-                                      <span class="help-block">
                                           <strong style="color:red">{{ $errors->first('alias') }}</strong>
-                                      </span>
                                   @endif
+                                </span>
                               </div>
                           </div>
                           <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
@@ -81,11 +81,11 @@
                               <div class="col-md-12">
                                   <input id="nombre" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" required>
 
+                                  <span class="help-block" style="color:red" id="errorNombre">
                                   @if ($errors->has('first_name'))
-                                      <span class="help-block">
                                           <strong style="color:red">{{ $errors->first('first_name') }}</strong>
-                                      </span>
                                   @endif
+                                </span>
                               </div>
                           </div>
 
@@ -95,11 +95,11 @@
                               <div class="col-md-12">
                                   <input id="apellido" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autofocus>
 
+                                  <span class="help-block" style="color:red" id="errorApellido">
                                   @if ($errors->has('last_name'))
-                                      <span class="help-block">
                                           <strong style="color:red">{{ $errors->first('last_name') }}</strong>
-                                      </span>
                                   @endif
+                                </span>
                               </div>
                           </div>
 
@@ -109,11 +109,11 @@
                               <div class="col-md-12">
                                   <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
+                                  <span class="help-block" style="color:red" id="errorEmail">
                                   @if ($errors->has('email'))
-                                      <span class="help-block">
                                           <strong style="color:red">{{ $errors->first('email') }}</strong>
-                                      </span>
                                   @endif
+                                </span>
                               </div>
                           </div>
 
@@ -123,11 +123,11 @@
                               <div class="col-md-12">
                                   <input id="password" type="password" class="form-control" name="password" required>
 
+                                  <span class="help-block" style="color:red" id="errorPassword">
                                   @if ($errors->has('password'))
-                                      <span class="help-block">
                                           <strong style="color:red">{{ $errors->first('password') }}</strong>
-                                      </span>
                                   @endif
+                                </span>
                               </div>
                           </div>
 
@@ -180,12 +180,69 @@
     var password = document.querySelector('#password');
     var password_confirm = document.querySelector('#password-confirm');
     var button = document.querySelector('#button');
-
+    var re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var error = 0;
     button.addEventListener('click',function() {
-      if (true) {
-
+      error = 0;
+      if (alias.value=="") {
+        document.querySelector('#errorAlias').innerText="El alias no puede estar vacio.";
+        error++
+      }else if(alias.value.length > 20) {
+        document.querySelector('#errorAlias').innerText='El alias no puede tener mas de 10 caracteres.';
+        error++
+      } else {
+        document.querySelector('#errorAlias').innerText= "";
       }
 
+      if (nombre.value=="") {
+        error++
+        document.querySelector('#errorNombre').innerText="El nombre no puede estar vacio.";
+      }else if(nombre.value.length > 20) {
+        error++
+        document.querySelector('#errorNombre').innerText='El nombre no puede tener mas de 20 caracteres.';
+      } else {
+        document.querySelector('#errorNombre').innerText= "";
+      }
+
+      if (apellido.value=="") {
+        error++
+        document.querySelector('#errorApellido').innerText="El apellido no puede estar vacio.";
+      }else if(apellido.value.length > 20) {
+        error++
+        document.querySelector('#errorApellido').innerText='El apellido no puede tener mas de 20 caracteres.';
+      }else {
+        document.querySelector('#errorApellido').innerText= "";
+      }
+
+      if (email.value=="") {
+        error++
+        document.querySelector('#errorEmail').innerText="El email no puede estar vacio.";
+      }else if(email.value.length > 100) {
+        error++
+        document.querySelector('#errorEmail').innerText='El email no puede tener mas de 100 caracteres.';
+      } else if (!re.test(email.value)) {
+        error++
+        document.querySelector('#errorEmail').innerText= "El email no es valido.";
+      } else {
+        document.querySelector('#errorEmail').innerText= "";
+      }
+
+      if (password.value == "") {
+        error++
+        document.querySelector('#errorPassword').innerText = "La contraseña no puede estar vacia."
+      } else if (password.value.length < 6) {
+        error++
+        document.querySelector('#errorPassword').innerText = "La contraseña debe ser mayor a 6 caracteres";
+      } else if(password.value!==password_confirm.value){
+        error++
+        document.querySelector('#errorPassword').innerText = "Las contraseñas no coinciden";
+      } else{
+        document.querySelector('#errorPassword').innerText = "";
+      }
+
+      if (error == 0) {
+        document.querySelector('#formRegister').submit();
+      }
     })
   </script>
 @endsection
