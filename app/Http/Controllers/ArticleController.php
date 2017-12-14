685715622480e96297376da787528eb53c5d3eaf
alegroni <ajmarzano@gmail.com>
2017-12-14 14:09:39 -0300

    MUCHOS CAMBIOS

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -191,6 +191,9 @@ class ArticleController extends Controller
       $post= Post::find($request->input('id_producto'));
         if (Auth::user()->id== $post->user_id) {
           $post->delete();
+
+          $eliminar = Fav::where('post_id','=',$request->input('id_producto'))->delete();
+
           return back();
         } else {
           return back();
