0b01ba3bc9026b62b7198f598b81ffadb7b2bb16
hugojos <hugosajama07@gmail.com>
2017-12-31 21:54:57 -0300

    carrousel pasado a jquery

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -296,34 +296,30 @@
     @endforeach
     </div>
 <script type="text/javascript">
-  var carrousel = document.querySelector('.carrousel')
-  var carrito = document.querySelector('.carrousel-images')
-
+  var $carrousel = $('.carrousel');
+  var $carrito = $('.carrousel-images');
   var imagenActual = 0
+  var cantidadImagenes = $('.carrousel img').length;
 
-  var cantidadImagenes = document.querySelectorAll('.carrousel img').length
-
-  document.querySelector('button.prev').addEventListener('click', function () {
+$('button.prev').click( function () {
       if (imagenActual > 0) {
         imagenActual--
       } else {
         imagenActual = cantidadImagenes - 1
       }
-        carrito.style.transform = 'translateX(' + (-carrousel.offsetWidth * imagenActual) + 'px)';
+        $carrito.css('transform','translateX(' + (-$carrousel.width() * imagenActual) + 'px)');
   })
 
-  document.querySelector('button.next').addEventListener('click', function () {
+  $('button.next').click( function () {
       if (imagenActual < (cantidadImagenes - 1)) {
         imagenActual++
       } else {
         imagenActual = 0
       }
-      carrito.style.transform = 'translateX(' + (-carrousel.offsetWidth * imagenActual) + 'px)';
+      $carrito.css('transform', 'translateX(' + (-$carrousel.width() * imagenActual) + 'px)');
   })
 
-  carrito.addEventListener('transitionend', function () {
-  console.log('fin de la transition')
-  })
+
 
   var $guardarCambios = $('.guardarCambios');
   var $cancelarCambios = $('.cancelarCambios');
